8bd63482a2e663ea2caf9fbc18698bdcdc9e994b
SebastianEscudero <sebastin.escudero@uc.cl>
2024-04-01 19:13:00 -0300

    convex config

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,10 +1,8 @@
-const config = {
-  providers: [
-    {
-      domain: "https://www.sketchlie.com/",
-      applicationID: process.env.AUTH_SECRET
-    },
-  ],
-};
-
-export default config;
+export default {
+    providers: [
+      {
+        domain: "https://www.sketchlie.com/",
+        applicationID: "sketchlie",
+      },
+    ],
+  };
