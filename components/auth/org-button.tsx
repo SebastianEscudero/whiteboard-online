ffc6784b205cc4b63a5882f768735736c03c8485
SebastianEscudero <sebastin.escudero@uc.cl>
2024-04-02 21:39:49 -0300

    style fixes

--- components/auth/org-button.tsx
+++ components/auth/org-button.tsx
@@ -17,7 +17,7 @@ export const NewOrgButton = ({
   return (
         <Dialog>
             <DialogTrigger asChild>
-                <div className="h-[36px] w-[36px]">
+                <div className="aspect-square">
                     <Hint label="Create Organization" side="right" align="start" sideOffset={18}>
                         <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
                             <Plus className="text-white"/>
