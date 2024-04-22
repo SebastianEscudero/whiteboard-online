fec2b24ac3a5ee671a48ecba3846d14a64c159a8
SebastianEscudero <sebastin.escudero@uc.cl>
2024-04-22 10:36:27 -0400

    added close button to toast

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -5,6 +5,7 @@ import { Room } from "@/components/room";
 import { Loading } from "./_components/loading";
 import { useEffect } from "react";
 import { toast } from "sonner";
+import { Mouse } from "lucide-react";
 
 interface BoardIdPageProps {
   params: {
@@ -18,8 +19,13 @@ const BoardIdPage = ({
   
   useEffect(() => {
     toast("Heads up!", {
-      description: "Right click and drag to move around the canvas!",
+      description: (
+        <p className="flex flex-row">
+          <Mouse size={16} className="mr-2"/> Right click and drag to move around the canvas!
+        </p>
+      ),
       position: "top-center",
+      closeButton: true
     });
   })
 
