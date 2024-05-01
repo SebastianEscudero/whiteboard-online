5d53e5ca74f73ff9975d505a0549b53cfed8355e
SebastianEscudero <sebastin.escudero@uc.cl>
2024-05-01 18:06:08 -0400

    fixed for real

--- app/auth/reset/page.tsx
+++ app/auth/reset/page.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   description: "La plataforma de colaboración donde puedes convertir tus ideas en realidad. Hecha para que equipos puedan colaborar, diseñar y innovar. Construye el futuro con nosotros",
   keywords: ["sketchlie register", "sketchlie sign up", "sketchlie", "sketchlie new password", "sketchlie reset password", "sketchlie contrasena",],
   alternates: {
-    canonical: "https://www.sketchlie.com/auth/new-password/",
+    canonical: "https://www.sketchlie.com/auth/reset/",
   }
 }
 
