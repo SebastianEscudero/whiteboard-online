6074303208b74abcc85d7bb08dee8a5cdb15866a
SebastianEscudero <sebastin.escudero@uc.cl>
2024-05-19 19:44:57 -0400

    added logic to creating orgs + boards when subscription expires

--- lib/planLimits.ts
+++ lib/planLimits.ts
@@ -4,42 +4,36 @@ export function getMaxBoards(org: any): number {
   const plan = org.subscriptionPlan;
 
   const planDetails = plans.find(subscriptionPlan => subscriptionPlan.label === plan);
+  
+  if (org.subscription) {
+    if (new Date(org.subscription.mercadoPagoCurrentPeriodEnd).getTime() < new Date().getTime()) {
+      return 1;
+    }
+  }
+
   return planDetails ? (planDetails.features.Boards === "Ilimitados" ? Infinity : Number(planDetails.features.Boards)) : 0;
 }
 
 export function getMaxCapas(org: any): number {
   const plan = org.subscriptionPlan;
-  
+
   const planDetails = plans.find(subscriptionPlan => subscriptionPlan.label === plan);
   return planDetails ? (planDetails.features["Capas máximas"] === "Ilimitados" ? Infinity : Number(planDetails.features["Capas máximas"])) : 0;
 }
 
 export function getMaxOrganizations(user: any): number {
-  let maxOrganizations = 0;
-  let hasStarterPlan = false;
-  let planLimit = 0;
-
+  let maxOrganizations = 1;
   for (const org of user.organizations) {
     const plan = org.subscriptionPlan;
     const planDetails = plans.find(subscriptionPlan => subscriptionPlan.label === plan);
-    planLimit = planDetails ? (planDetails.features["Teams"] === "Ilimitados" ? Infinity : Number(planDetails.features["Teams"])) : 0;
-
-    if (planDetails) {
-      if (plan === 'Business') {
-        return planLimit; 
-      } else if (plan === 'Starter') {
-        maxOrganizations = planLimit;
-        hasStarterPlan = true;
-      }
-    }
+    let planLimit = 1
+    if (org.subscription) {
+      if (new Date(org.subscription.mercadoPagoCurrentPeriodEnd).getTime() > new Date().getTime()) {
+        planLimit = planDetails ? (planDetails.features["Teams"] === "Ilimitados" ? Infinity : Number(planDetails.features["Teams"])) : 0;
+    }}
+    maxOrganizations = Math.max(maxOrganizations, planLimit);
   }
-
-  if (hasStarterPlan) {
-    return maxOrganizations; 
-  }
-
-  const gratisPlanDetails = plans.find(subscriptionPlan => subscriptionPlan.label === 'Gratis');
-  return gratisPlanDetails ? (gratisPlanDetails.features["Teams"] === "Ilimitados" ? Infinity : Number(gratisPlanDetails.features["Teams"])) : 0;
+  return maxOrganizations;
 }
 
 export function getMaxImageSize(org: any): number {
