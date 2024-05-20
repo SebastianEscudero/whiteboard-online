01d9cd3d2672fde49ee1835035fba5f319afe9d4
SebastianEscudero <sebastin.escudero@uc.cl>
2024-05-19 23:42:05 -0400

    text resize works!

--- lib/utils.ts
+++ lib/utils.ts
@@ -120,8 +120,8 @@ export function resizeBounds(
     }
   }
 
-  if (layer && layer?.height/layer?.width === result.height/result.width && textareaRef && textareaRef.current && layer.type === LayerType.Text) {
-    const newFontSize = Math.min(result.width/layer.width, result.height/layer.height ) * layer.textFontSize
+  if (layer && Math.abs((layer?.height / layer?.width) - (result.height / result.width)) < 0.01 && textareaRef && textareaRef.current && layer.type === LayerType.Text) {
+    const newFontSize = result.width / layer.width * layer.textFontSize
     result.textFontSize = newFontSize
     return result
   }
