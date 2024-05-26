fc0edeeb020ab21f508e6a2cf1aa4e94333657b2
SebastianEscudero <sebastin.escudero@uc.cl>
2024-05-26 12:55:58 -0400

    new landing page

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -1,50 +1,15 @@
 "use client";
 
-import Image from 'next/image';
-import { Button } from './ui/button';
-import Link from 'next/link';
-import TypewriterComponent from 'typewriter-effect';
+import { BlogStructure } from './blog-structure';
 
 export const LandingHero = () => {
     return (
-        <div className="relative flex h-screen w-full overflow-hidden" style={{ backgroundImage: "url(/dot-grid.png)", backgroundSize: 'cover' }}>
-            <Image src="/hero-bg.png" alt="hero bg" className="absolute left-0 top-16 z-10 hidden h-full w-full object-cover sm:flex" width={4000} height={4000}/>
-            <Image src="/hero-bg-mobile.png" alt="hero bg" className="absolute left-0 top-0 z-10 h-full w-full object-cover sm:hidden" width={4000} height={4000} />
-            <div className="bg-black-10 absolute left-0 top-0 z-20 h-full w-full"></div>
-            <div className="z-40 flex h-full w-full items-center justify-center gap-6 px-6 text-black" id="home">
-                <div className="flex flex-col items-center justify-start gap-3">
-                    <h1 className="max-w-[800px] text-center text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-roobert">
-                        Dibuja tus sueños y conviértelos en realidad
-                        <div className="text-center bg-clip-text text-[#2E4DE6] leading-small mt-2">
-                        <TypewriterComponent 
-                            options = {{
-                                strings: [
-                                    "Colabora.",
-                                    "Diseña.",
-                                    "Crea.",
-                                    "Enseña.",
-                                ],
-                                autoStart: true,
-                                loop: true,
-                            }}
-                        />
-                    </div>
-                    </h1>
-                    <p className="max-w-[800px] text-center text-gray-600 text-lg sm:text-xl mb-2 font-roobert">Haz una lluvia de ideas, colabora y da vida a tus ideas en nuestro espacio de colaboración en tiempo real. Únete a nosotros y convierte tus ideas en realidad.</p>
-                    <div className="flex gap-4">
-                        <Link href="/auth/register/">
-                            <Button variant="auth" className="p-5 text-[16px] md:p-6 text-lg">
-                                Regístrate gratis
-                            </Button>
-                        </Link>
-                        <a href="#about">
-                            <Button variant="landing" className="p-5 text-[16px] md:p-6 text-lg">
-                                Aprende más
-                            </Button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <BlogStructure 
+            title="Dibuja tus sueños y conviértelos en realidad"
+            description="Haz una lluvia de ideas, colabora y da vida a tus ideas en nuestro espacio de colaboración en tiempo real. Únete a nosotros y convierte tus ideas en realidad."
+            cta="Regístrate gratis"
+            img="/placeholders/linea-de-tiempo.png"
+            alt="Linea de tiempo"
+        />
     )
 }
