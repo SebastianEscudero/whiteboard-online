42d7b2f0a990b5cca9dc4b1f181d8ec3cbc26f2a
SebastianEscudero <sebastin.escudero@uc.cl>
2024-06-03 16:43:43 -0400

    trying to fix lcs

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -9,15 +9,15 @@ import { Button } from "./ui/button";
 export const LandingNavbar = () => {
     return (
         <nav className="py-3 bg-[#FFFFFF] border-b border-zinc-600 sticky top-0 z-50 h-[71px]">
-            <div className="flex items-center justify-between xl:mx-[5%] lg:mx-[3%] md:mx-[2%] mx-[1%]">
+            <div className="flex items-center justify-between xl:mx-[5%] lg:mx-[3%] md:mx-[2%] mx-[1%] h-[40px]">
                 <div className="flex items-center">
                     <MobileSidebar />
-                    <Link href="/" className="flex items-center mr-2 ml-2">
+                    <Link href="/" className="flex items-center mr-2 ml-2" title="Sketchlie">
                         <div className="mr-4 h-full w-full">
                             <Image
                                 height={65}
                                 width={65}
-                                alt="Logo"
+                                alt="Sketchlie"
                                 src="/logo.svg"    
                                 loading="lazy"
                             />
@@ -29,7 +29,7 @@ export const LandingNavbar = () => {
                     <NavigationMenuLanding />
                 </div>
                 <div className="hidden sm:flex items-center gap-x-2">
-                    <Link href="/auth/register/">
+                    <Link href="/auth/register/" title="Regístrate gratis">
                         <Button variant="auth" className="rounded-lg">
                             Regístrate gratis
                         </Button>
